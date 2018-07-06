9732036bdf0ea26b728d14ac486d1fa3a4057929
Luis Jimenez Gonzales <ljimnezgo@unsa.edu.pe>
2018-07-06 01:13:44 -0500

    usuarios

--- PIS/Vista/perfil-alumno.php
+++ PIS/Vista/perfil-alumno.php
@@ -3,6 +3,81 @@
 <html>
 <?php include("head.php"); ?>
 <body>
+<?php
+if($_SESSION['rol']==2){
+?>
+        <!-- Navigation -->
+        <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="bienvenida.php">Bienestar Social</a>
+            </div>
+            <!-- /.navbar-header -->
+
+            <ul class="nav navbar-top-links navbar-right">
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                        <?php
+							echo "<i>" . $_SESSION['usuario_cuenta']."</i>";
+						?>
+						<i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i>
+                    </a>
+                    <ul class="dropdown-menu dropdown-user">
+                        <li><a href="perfil-alumno.php"><i class="fa fa-user fa-fw"></i> Perfil</a>
+                        </li>
+                        <li><a href="cambio-contrasena.php"><i class="fa fa-gear fa-fw"></i> Configuración</a>
+                        </li>
+                        <li class="divider"></li>
+                        <li><a href="logout.php"><i class="fa fa-sign-out fa-fw"></i> Cerrar Sesión</a>
+                        </li>
+                    </ul>
+                    <!-- /.dropdown-user -->
+                </li>
+                <!-- /.dropdown -->
+            </ul>
+            <!-- /.navbar-top-links -->
+
+            <div class="navbar-default sidebar" role="navigation">
+                <div class="sidebar-nav navbar-collapse">
+                    <ul class="nav" id="side-menu">
+						<li>
+                            <a href="perfil-alumno.php"><i class="fa fa-wrench fa-fw"></i> Perfil</a>
+                        </li>
+											
+						
+						<!-- /Relaciones públicas y dirección -->                       
+						<li>
+                            <a href="#"><i class="fa fa-files-o fa-fw"></i> Malla Curricular<span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                                <li>
+                                    <a href="lista-malla.php">Malla</a>
+                                </li>
+
+                            </ul>
+                            <!-- /.nav-second-level -->
+                        </li>
+						<!-- /Malla Curricular -->		
+					
+						
+                    </ul>
+                </div>
+                <!-- /.sidebar-collapse -->
+            </div>
+            <!-- /.Barra Desplegable Izquierda -->
+        </nav>
+		<!-- /.Barra Alumno -->
+<?php
+}
+else{
+?>
+
+
+
 
     <div id="wrapper">
 	<?php include("panel.php"); ?>
@@ -109,6 +184,11 @@
         <!-- /#page-wrapper -->
 
     </div>
+
+
+<?php
+}
+?>
     <!-- /#wrapper -->
 	<?php include("scripts.php"); ?>
 </body>
