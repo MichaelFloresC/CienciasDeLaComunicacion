a17f4e30ae91d1d4fd6fb022b314aba49fb5123c
Luis Jimenez Gonzales <ljimnezgo@unsa.edu.pe>
2018-07-13 00:51:08 -0500

    Casi Final

--- PIS/Vista/panel.php
+++ PIS/Vista/panel.php
@@ -18,7 +18,7 @@ if($_SESSION['rol']==1){
                 <li class="dropdown">
                     <a class="dropdown-toggle" data-toggle="dropdown" href="#">
                         <?php
-							echo "<i>" . $_SESSION['usuario_cuenta']."</i>";
+							echo "<i>" . $_SESSION['nombre_persona']."</i>";
 						?>
 						<i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i>
                     </a>
@@ -131,7 +131,7 @@ if($_SESSION['rol']==2){
                 <li class="dropdown">
                     <a class="dropdown-toggle" data-toggle="dropdown" href="#">
                         <?php
-							echo "<i>" . $_SESSION['usuario_cuenta']."</i>";
+							echo "<i>" . $_SESSION['nombre_persona']."</i>";
 						?>
 						<i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i>
                     </a>
@@ -200,7 +200,7 @@ if($_SESSION['rol']==3){
                 <li class="dropdown">
                     <a class="dropdown-toggle" data-toggle="dropdown" href="#">
                         <?php
-							echo "<i>" . $_SESSION['usuario_cuenta']."</i>";
+							echo "<i>" . $_SESSION['nombre_persona']."</i>";
 						?>
 						<i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i>
                     </a>
